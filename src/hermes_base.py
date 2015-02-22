d3b9841667cfd94a65e047e3389c7e9f9d46bdd1
AlvinCarl <liuyuantuo@gmail.com>
2015-02-22 09:55:52 +0800

    add for hermes project

---
+++ src/hermes_base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+# __author__ = 'liuyuantuo@gmail.com'
+
+"""
+    base class of hermes, include module:
+    1.configure reader interface
+    2.logger interface
+    3.product version manager interface
+    4.exception interface
+
+"""
+
+
+class hermes_base(object):
+    """
+        base class
+
+    """
+    def __init__(self):
+        pass
